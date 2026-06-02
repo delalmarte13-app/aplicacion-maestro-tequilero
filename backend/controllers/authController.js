4b37a87b8842fb485dab998a64d44be115ab9cb5
delalmarte13-app <delalmarte13@gmail.com>
2026-06-01 23:33:53 -0600

    Crear ruta auth

---
+++ backend/controllers/authController.js
@@ -0,0 +1,11 @@
+export const login = async (req, res) => {
+  res.json({
+    mensaje: "Login pendiente"
+  });
+};
+
+export const register = async (req, res) => {
+  res.json({
+    mensaje: "Registro pendiente"
+  });
+};
